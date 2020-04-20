de9a33df3b4a3c1c4c6ee0d38a935a9bde9b3f71
MD HASIBUR RAHMAN LEMON <58658662+hasiburrahman123@users.noreply.github.com>
2020-04-20 09:09:45 +0800

    Add files via upload
    Here i submit URI online judge 1013 solution . Here i change something by my own .

---
+++ URI_1013_solution.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+int main(){
+ int A , B , C , temp ;
+
+ scanf("%d %d %d" , &A ,&B ,&C);
+
+temp = A ;
+ if(A < B || A < C){
+    if (B < C ){
+        temp = C;
+    }
+    else{
+        temp = B;
+    }
+
+ }
+
+ printf("Heighest number is : %d\n", temp);
+}
+
+
